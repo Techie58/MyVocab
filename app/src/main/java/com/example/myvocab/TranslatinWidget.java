8f0bd9b9e4611a2ceac47fc606d4720a8aaaf6bf
Developer <techie11.2022@gmail.com>
2023-11-01 12:53:26 +0500

    Updated Widget

--- app/src/main/java/com/example/myvocab/TranslatinWidget.java
+++ app/src/main/java/com/example/myvocab/TranslatinWidget.java
@@ -1,22 +1,24 @@
 package com.example.myvocab;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class TranslatinWidget extends AppWidgetProvider {
-
+    static RemoteViews views;
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.translatin_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views = new RemoteViews(context.getPackageName(), R.layout.translatin_widget);
+        views.setTextViewText(R.id.widget_btn_1, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -27,6 +29,11 @@ public class TranslatinWidget extends AppWidgetProvider {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
+            Intent intent=new Intent(context,MainActivity.class);
+            PendingIntent pendingIntent=PendingIntent.getActivity(context,0,intent, PendingIntent.FLAG_IMMUTABLE);
+
+            views.setOnClickPendingIntent(R.id.widget_btn_1,pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetId,views);
         }
     }
 
