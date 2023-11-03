5219a5aab648608f1cfeb9fa99d1792c13316d41
Developer <techie11.2022@gmail.com>
2023-11-03 12:10:46 +0500

    Stackview widget added

---
+++ app/src/main/java/com/example/myvocab/StackView.java
@@ -0,0 +1,51 @@
+package com.example.myvocab;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class StackView extends AppWidgetProvider {
+
+    static Intent serviceIntent;
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            serviceIntent=new Intent(context,StackViewService.class);
+            serviceIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,appWidgetId);
+            serviceIntent.setData(Uri.parse(serviceIntent.toUri(Intent.URI_INTENT_SCHEME)));
+
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+            int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.stak_view);
+        views.setTextViewText(R.id.stackViewEmptyTxt, widgetText);
+        views.setRemoteAdapter(R.id.stackView,serviceIntent);
+        views.setEmptyView(R.id.stackView,R.id.stackViewEmptyTxt);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+}
---
+++ app/src/main/java/com/example/myvocab/StackViewService.java
@@ -0,0 +1,75 @@
+package com.example.myvocab;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.example.myvocab.R;
+
+public class StackViewService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new StackViewItemFactory(getApplicationContext(),intent);
+    }
+    static class StackViewItemFactory implements RemoteViewsFactory{
+        private Context context;
+        private int appWidgetId;
+        private String[] myData={"One","Two","Three"};
+
+        public StackViewItemFactory(Context context,Intent intent) {
+            this.context = context;
+            this.appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        @Override
+        public void onCreate() {
+
+        }
+
+        @Override
+        public void onDataSetChanged() {
+
+        }
+
+        @Override
+        public void onDestroy() {
+
+        }
+
+        @Override
+        public int getCount() {
+            return myData.length;
+        }
+
+        @Override
+        public RemoteViews getViewAt(int i) {
+            RemoteViews remoteViews=new RemoteViews(context.getPackageName(), R.layout.stack_view_layout);
+            remoteViews.setTextViewText(R.id.stackViewEmptyTxt,myData[i]);
+            SystemClock.sleep(500);
+            return remoteViews;
+        }
+
+        @Override
+        public RemoteViews getLoadingView() {
+            return null;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 1;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+    }
+}
